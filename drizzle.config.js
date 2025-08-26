012811922a48f6c8ed511e1a910b3e8ce2c39b49
AveryOn <lnkonion2@gmail.com>
2025-08-26 22:42:36 +0400

    fix

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,11 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const drizzle_kit_1 = require("drizzle-kit");
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const drizzle_kit_1 = require('drizzle-kit');
 exports.default = (0, drizzle_kit_1.defineConfig)({
-    schema: './src/db/schema.ts',
-    out: './drizzle',
-    dialect: 'sqlite',
-    dbCredentials: {
-        url: './data/db.sqlite',
-    },
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: './data/db.sqlite',
+  },
 });
